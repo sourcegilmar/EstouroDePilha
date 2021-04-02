2767fdead2d8d02b38fe51678679b61af4672688
sourcegilmar <gilmardw@gmail.com>
2021-04-02 16:38:38 -0300

    projeto criado

---
+++ src/EstouroDePilha.java
@@ -0,0 +1,15 @@
+
+public class EstouroDePilha {
+	static int q = 0;
+	public static void main(String[] args) {
+		boo();
+	}	
+	public static void boo() {
+		q++;
+		// Put a lower valor to q in order to avoid:
+		// Exception in thread "main" java.lang.StackOverflowError
+		if (q>25)
+			System.exit(0);
+		boo();
+	}	
+}
